2c8c6d28e0ecb07830307734a3052c552855e3ad
Adrian Edwards <adrian@adriancedwards.com>
2024-10-09 19:40:30 -0400

    ChatGPT assisted - wrap in provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux'; // Ensure you import Provider
+import { store } from './app/store'; // Import your Redux store
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css'; // Or your global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
